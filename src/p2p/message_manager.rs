f7b8b0d0b49636ac3829720eb2a4532871639d34
matken11235 <26405363+matken11235@users.noreply.github.com>
2019-06-14 21:17:11 +0900

    Disband a struct MessageManager

--- src/p2p/message_manager.rs
+++ src/p2p/message_manager.rs
@@ -34,42 +34,31 @@ pub struct Message {
     pub payload: Option<Vec<SocketAddr>>,
 }
 
-#[derive(Clone)]
-pub struct MessageManager;
-
-impl MessageManager {
-    pub fn new() -> MessageManager {
-        println!("Initializing MessageManager ...");
-        MessageManager{}
-    }
-
-    pub fn build(&self, msg_type: MsgType, my_addr: SocketAddr, payload: Option<Vec<SocketAddr>>) -> String {
-        let msg = Message {
+impl Message {
+    pub fn new(msg_type: MsgType, my_addr: SocketAddr, payload: Option<Vec<SocketAddr>>) -> Message {
+        Message {
             protocol: PROTOCOL_NAME.to_string(),
             version: PROTOCOL_VERSION.to_string(),
             msg_type,
             my_addr,
             payload,
-        };
-        serde_json::to_string(&msg).unwrap()
-    }
-
-    pub fn parse(&self, msg_str: &String) -> Result<Message, &'static str> {
-        let msg: Message = serde_json::from_str(&msg_str).unwrap();
-
-        if msg.protocol != PROTOCOL_NAME.to_string() {
-            Err("Protocol name is not matched")
-        } else if Version::parse(&msg.version) > Version::parse(PROTOCOL_VERSION) {
-            Err("Protocol version is not matched")
-        } else {
-            Ok(msg)
         }
     }
 }
 
-impl Drop for MessageManager {
-    /// Close socket.
-    fn drop(&mut self) -> () { // connection_close
-        println!("Shutdown MessageManager ...");
+pub fn build(msg_type: MsgType, my_addr: SocketAddr, payload: Option<Vec<SocketAddr>>) -> String {
+    let msg = Message::new(msg_type, my_addr, payload);
+    serde_json::to_string(&msg).unwrap()
+}
+
+pub fn parse(msg_str: &String) -> Result<Message, &'static str> {
+    let msg: Message = serde_json::from_str(msg_str).unwrap();
+
+    if msg.protocol != PROTOCOL_NAME.to_string() {
+        Err("Protocol name is not matched")
+    } else if Version::parse(&msg.version) > Version::parse(PROTOCOL_VERSION) {
+        Err("Protocol version is not matched")
+    } else {
+        Ok(msg)
     }
 }
